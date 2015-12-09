f9193c55fff2f91a15dfedb667ab7338f2a59b71
Joran Elias <joran.elias@umontana.edu>
2015-12-09 13:24:10 -0700

    basic db/query functions with better names

---
+++ R/db_functions.R
@@ -0,0 +1,31 @@
+#' Create Local XC DB Connection
+#'
+#' @export
+#' @import RSQLite
+db_xc_local <- function(){
+  dbConnect(SQLite(), statskier2::sqlite_path)
+}
+
+#' Create Remote XC DB Connection
+#'
+#' @export
+#' @import RMySQL
+db_xc_remote <- function(){
+  dbConnect(drv = RMySQL::MySQL(),
+            dbname = options()$mysql$dbName,
+            username = options()$mysql$user,
+            password = options()$mysql$password,
+            host = options()$mysql$host,
+            port = options()$mysql$port)
+}
+
+#' Query XC Database
+#'
+#' @param con database connection
+#' @param q character; SQL
+#'
+#' @import DBI
+#' @export
+ss_query <- function(con,q){
+  dbGetQuery(con,q)
+}
