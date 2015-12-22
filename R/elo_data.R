89bbfdbdcb5efaa68f4d9e6b26d33b4f5a789971
Joran Elias <joran.elias@umontana.edu>
2015-12-22 15:00:46 -0700

    functions to create starting data for ELO functions

---
+++ R/elo_data.R
@@ -0,0 +1,55 @@
+#' Generate ELO Distance Input Data
+#'
+#' @param default_rating
+#' @export
+dst_elo_data <- function(default_rating = 1300){
+  src <- src_sqlite(path = statskier2:::sqlite_path,create = FALSE)
+
+  dst <- tbl(src = src,"main") %>%
+    filter(type == 'Distance') %>%
+    arrange(date,raceid,rank) %>%
+    collect()
+
+  current_rating_dst <- dst %>%
+    select(gender,fisid,name,nation,date) %>%
+    unique() %>%
+    group_by(gender,fisid,name) %>%
+    summarise(nation = nation[date == max(date)][1])
+  current_rating_dst$race_count <- 0L
+  current_rating_dst$cur_rating <- default_rating
+
+  raceid_order <- unique(dst$raceid)
+  dst <- split(dst,dst$raceid)
+  dst <- dst[as.character(raceid_order)]
+
+  return(list(races = dst,
+              init_rating = current_rating_dst))
+}
+
+#' Generate ELO Sprint Input Data
+#'
+#' @param default_rating
+#' @export
+spr_elo_data <- function(default_rating = 1300){
+  src <- src_sqlite(path = statskier2:::sqlite_path,create = FALSE)
+
+  spr <- tbl(src = src,"main") %>%
+    filter(type == 'Sprint') %>%
+    arrange(date,raceid,rank) %>%
+    collect()
+
+  current_rating_spr <- spr %>%
+    select(gender,fisid,name,nation,date) %>%
+    unique() %>%
+    group_by(gender,fisid,name) %>%
+    summarise(nation = nation[date == max(date)][1])
+  current_rating_spr$race_count <- 0L
+  current_rating_spr$cur_rating <- default_rating
+
+  raceid_order <- unique(spr$raceid)
+  spr <- split(spr,spr$raceid)
+  spr <- spr[as.character(raceid_order)]
+
+  return(list(races = spr,
+              init_rating = current_rating_spr))
+}
