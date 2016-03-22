fe7b61a5d73271da3dbb83a31404da6fa65ca2ed
Joran Elias <joran.elias@umontana.edu>
2016-03-22 11:16:40 -0600

    switch to use common connection

--- R/name_search.R
+++ R/name_search.R
@@ -1,9 +1,10 @@
 #' @export
 find_skier <- function(x){
-  conl <- db_xc_local()
-  q <- sprintf("select fisid,name,gender,yob,nation,count(distinct raceid) races
-  from main where name like '%%%s%%' group by fisid,name,gender,yob,nation",x)
-  results <- ss_query(conl,q)
-  dbDisconnect(conl)
+  x <- paste0("%",x,"%")
+  results <- tbl(src = options()$statskier_src,"main") %>%
+    select(fisid,compid,name,gender,nation) %>%
+    filter(name %like% x) %>%
+    collect() %>%
+    unique()
   results
 }
