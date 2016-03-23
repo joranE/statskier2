ff355128565a8fbe82ddb91cf2f8957213194162
joran <joran.elias@gmail.com>
2016-03-22 19:28:55 -0600

    holy crap. turns out when stat = "identity" in a stacked bar plot, the order of the filled segments respects the order of the data, not the order of the factor levels. Good grief.

--- R/spr_plot.R
+++ R/spr_plot.R
@@ -46,30 +46,30 @@ spr_plot <- function(ath_names,by_tech = FALSE){
     group_by_(.dots = grps) %>%
     summarise(n_result = n()) %>%
     mutate(level = factor(level,
-                          levels = c('Qual','Final','Semi','Quarter'),
-                          labels = paste(c(1,4:2),c('Qual','Final','Semi','Quarter'))))
+                          levels = c('Qual','Quarter','Semi','Final')))
 
-  lower <- filter(ath_data,level == '1 Qual')
-  upper <- filter(ath_data,level != '1 Qual')
+  lower <- filter(ath_data,level == 'Qual')
+  upper <- filter(ath_data,level != 'Qual')
 
   upper$level <- factor(upper$level,levels = rev(levels(upper$level)))
+  upper <- arrange(upper,name,season,desc(level))
 
   p <- ggplot() +
     facet +
-    geom_bar(data = upper,
-             aes(x = season,y = n_result,fill = level,order = level),
-             width = 0.5,
-             position = "stack",
-             stat = "identity") +
     geom_bar(data = lower,
              aes(x = season,y = -n_result,fill = level),
              width = 0.5,
              position = "stack",
              stat = "identity") +
+    geom_bar(data = upper,
+             aes(x = season,y = n_result,fill = level,order = level),
+             width = 0.5,
+             position = "stack",
+             stat = "identity") +
     geom_hline(yintercept = 0,color = "black") +
     labs(x = NULL,y = "Number of Races",fill = "Max\nround\nreached") +
-    scale_fill_manual(values = RColorBrewer::brewer.pal(6,"Blues")[3:6],
-                      breaks = c('4 Final','3 Semi','2 Quarter','1 Qual')) +
+    scale_fill_manual(values = RColorBrewer::brewer.pal(6,"Blues")[c(6,3:5)],
+                      breaks = c('Final','Semi','Quarter','Qual')) +
     theme(axis.text.x = element_text(hjust = 0,vjust = 1,angle = 310))
 
   return(list(plot = p,
