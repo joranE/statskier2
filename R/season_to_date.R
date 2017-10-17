d1da1a291ac5792aca6b3738a971f23bc044e897
Joran Elias <joran.elias@umontana.edu>
2017-10-17 10:40:38 -0600

    needed this function back

---
+++ R/season_to_date.R
@@ -0,0 +1,12 @@
+#' Convert season string to date
+#'
+#' Converts season string like \code{2010-2011} to a date string like
+#' \code{2011-01-01}.
+#'
+#' @param x character; season to convert
+#'
+#' @export
+season_to_date <- function(x){
+  rs <- paste0(substr(x,6,9),"-01-01")
+  rs
+}
