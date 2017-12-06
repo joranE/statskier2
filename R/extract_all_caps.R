237e94f15c6ac5bde9a8ad4b80601d9c91630792
joran <joran.elias@gmail.com>
2017-12-06 15:04:30 -0700

    add shorten_names() function

--- R/extract_all_caps.R
+++ R/extract_all_caps.R
@@ -7,3 +7,16 @@ extract_all_caps <- function(x){
   stringr::str_extract(string = x,pattern = "([A-Z-]+[:space:])+") %>%
     stringr::str_trim()
 }
+
+#' Shorten Skier Names
+#'
+#' Grabs the all caps portion and applies title case
+#'
+#' @param x character vector of skier names
+#' @importFrom stringr str_to_title
+#' @export
+shorten_names <- function(x){
+  x %>%
+    stringr::str_extract(string = .,pattern = "^[A-Z]*") %>%
+    stringr::str_to_title()
+}
