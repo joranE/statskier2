ed350127a972a1ab35d30da8e7eddf66dde89d4a
joran <joran.elias@gmail.com>
2020-02-28 20:09:53 -0700

    more cleanup

--- R/head_to_head_data.R
+++ R/head_to_head_data.R
@@ -1,37 +1,4 @@
 #' @export
-hth_data <- function(athletes,opponents){
-  conl <- db_xc_local()
-  hth <- sq_file("sql/hth_data.sql") %>%
-    sq_set(ath_name = IN(athletes),
-           opp_name = IN(opponents)) %>%
-    sq_send(.with = ss_query_,conl)
-  dbDisconnect(conl)
-
-  winning_times <- tbl(src = options()$statskier_src,"main") %>%
-    filter(raceid %in% hth$raceid & rank == 1) %>%
-    select(raceid,winning_time = time) %>%
-    distinct() %>%
-    collect()
-
-  hth <- left_join(hth,
-                   winning_times,
-                   by = 'raceid') %>%
-    mutate(ath_pb = (ath_time - winning_time) / winning_time,
-           opp_pb = (opp_time - winning_time) / winning_time) %>%
-    mutate(diff_pb = opp_pb - ath_pb,
-           diff_rank = opp_rank - ath_rank,
-           diff_fispoints = opp_fispoints - ath_fispoints,
-           diff_rankqual = opp_rankqual - ath_rankqual)
-
-  hth <- hth %>%
-    group_by(ath_compid,opp_compid) %>%
-    mutate(n_races = n_distinct(raceid)) %>%
-    as.data.frame()
-
-  return(hth)
-}
-
-#' @export
 dst_hth_data <- function(ath_compid,opp_compid){
   result <- sq_file("sql/skier_head_to_head_dst.sql") %>%
     sq_set(ath_compid = ath_compid,
--- R/recent_races.R
+++ R/recent_races.R
@@ -1,7 +1,7 @@
 #' Recent Races
 #'
 #' Utility function for listing the most recent
-#' races in the database since a given date. Used mainly
+#' events in the database since a given date. Used mainly
 #' to help track what new races need to be entered.
 #'
 #' @param cur_date character; a date in the format "YYYY-MM-DD"
@@ -9,14 +9,14 @@
 #' @export
 #' @examples
 #' \dontrun{
-#' x <- recent_races("2016-02-01")
+#' x <- recent_events("2016-02-01")
 #' head(x)
 #' }
-recent_races <- function(cur_date){
-  res <- tbl(src = options()$statskier_src,"main") %>%
+recent_events <- function(cur_date){
+  res <- tbl(src = conl,"v_event") %>%
     filter(date >= cur_date) %>%
-    select(raceid,date,location,cat1,cat2,gender,type,tech,length,start) %>%
-    arrange(date,location,cat1,cat2,type,tech) %>%
+    select(eventid,date,location,event_type,cat1,cat2,gender,tech,length,format) %>%
+    arrange(date,location) %>%
     collect() %>%
     unique() %>%
     as.data.frame()
@@ -25,20 +25,20 @@ recent_races <- function(cur_date){
 
 #' Find Races
 #'
-#' Search for races on a specific date
+#' Search for events on a specific date
 #'
 #' @param date character; e.g. 'YYYY-MM-DD'
 #' @return A data frame with information on every race for the given date
 #' @export
 #' @examples
 #' \dontrun{
-#' find_races(cur_date = '2015-01-01')
+#' find_events(cur_date = '2015-01-01')
 #' }
-find_races <- function(cur_date){
-  res <- tbl(src = options()$statskier_src,"main") %>%
+find_events <- function(cur_date){
+  res <- tbl(conl,"v_event") %>%
     filter(date == cur_date) %>%
-    select(raceid,date,location,cat1,cat2,gender,type,tech,length,start) %>%
-    arrange(date,location,cat1,cat2,type,tech,gender) %>%
+    select(eventid,date,location,event_type,cat1,cat2,gender,tech,length,format) %>%
+    arrange(date,location) %>%
     collect() %>%
     unique() %>%
     as.data.frame()
