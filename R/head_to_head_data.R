83f93c2101386a675792959f7cede630eba465e3
joran <joran.elias@gmail.com>
2021-09-03 12:12:55 -0600

    head to head sprint heats

--- R/head_to_head_data.R
+++ R/head_to_head_data.R
@@ -36,6 +36,24 @@ spr_hth_data <- function(ath_compid,opp_compid){
 }
 
 #' @export
+spr_heats_hth_data <- function(ath_compid,opp_compid){
+  if (length(ath_compid) == 0 || length(ath_compid) > 1){
+    stop("Length of 'ath_compid' must be at least 1.")
+  }
+  if (length(opp_compid) < 1){
+    stop("Length of 'opp_compid' must be at least 1.")
+  }
+  sql <- read_sql("inst/sql/skier_head_to_head_spr_heats.sql")
+  sql <- glue::glue_sql(sql,
+                        ath_compid = ath_compid,
+                        opp_compid = opp_compid,
+                        .con = ..statskier_pg_con..)
+  result <- RPostgres::dbGetQuery(..statskier_pg_con..,sql) %>%
+    dplyr::mutate_if(.predicate = bit64::is.integer64,.funs = as.integer)
+  result
+}
+
+#' @export
 common_opponents <- function(ath_compid1,ath_compid2,min_date,max_date){
   if (length(ath_compid1) != 1){
     stop("Length of 'ath_compid1' must be 1.")
