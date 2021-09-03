e6ad61885738faf14d0b1aae359be2c33f8444c4
joran <joran.elias@gmail.com>
2021-09-03 12:13:34 -0600

    more flexible wyas to retrieve data

--- R/skier_results.R
+++ R/skier_results.R
@@ -1,16 +1,38 @@
 #' @export
-skier_results <- function(.name = NULL,.fisid = NULL,.compid = NULL){
+skier_results <- function(name = NULL,fisid = NULL,compid = NULL){
   v_dst <- dplyr::tbl(src = ..statskier_pg_con..,dbplyr::in_schema("public","v_distance"))
   v_spr <- dplyr::tbl(src = ..statskier_pg_con..,dbplyr::in_schema("public","v_sprint"))
 
-  dst_res <- v_dst %>%
-    filter(fisid %in% .fisid) %>%
-    collect() %>%
-    mutate_if(.predicate = bit64::is.integer64,.funs = as.integer)
-  spr_res <- v_spr %>%
-    filter(fisid %in% .fisid) %>%
-    collect() %>%
-    mutate_if(.predicate = bit64::is.integer64,.funs = as.integer)
+  if (!is.null(fisid)){
+    dst_res <- v_dst %>%
+      filter(fisid %in% local(fisid)) %>%
+      collect() %>%
+      mutate_if(.predicate = bit64::is.integer64,.funs = as.integer)
+    spr_res <- v_spr %>%
+      filter(fisid %in% local(fisid)) %>%
+      collect() %>%
+      mutate_if(.predicate = bit64::is.integer64,.funs = as.integer)
+  }
+  if (!is.null(compid)){
+    dst_res <- v_dst %>%
+      filter(fisid %in% local(compid)) %>%
+      collect() %>%
+      mutate_if(.predicate = bit64::is.integer64,.funs = as.integer)
+    spr_res <- v_spr %>%
+      filter(fisid %in% local(compid)) %>%
+      collect() %>%
+      mutate_if(.predicate = bit64::is.integer64,.funs = as.integer)
+  }
+  if (!is.null(name)){
+    dst_res <- v_dst %>%
+      filter(fisid %in% local(name)) %>%
+      collect() %>%
+      mutate_if(.predicate = bit64::is.integer64,.funs = as.integer)
+    spr_res <- v_spr %>%
+      filter(fisid %in% local(name)) %>%
+      collect() %>%
+      mutate_if(.predicate = bit64::is.integer64,.funs = as.integer)
+  }
 
   list(dst = dst_res,spr = spr_res)
 }
