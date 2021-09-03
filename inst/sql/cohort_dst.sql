4a5876c6e1328a8c65ed412a90f589c63fb11192
joran <joran.elias@gmail.com>
2021-09-03 12:14:08 -0600

    more flexible queries

--- inst/sql/cohort_dst.sql
+++ inst/sql/cohort_dst.sql
@@ -2,7 +2,7 @@ select vd.*
 from v_distance vd,
       (select compid
       from v_distance_maj_int
-      where rank <= {rnk}
+      where rank <= {rnk} and age <= {age}
       group by compid
       having count(distinct eventid) >= {topn}) tp
 where vd.compid = tp.compid
--- inst/sql/cohort_spr.sql
+++ inst/sql/cohort_spr.sql
@@ -3,7 +3,7 @@ select vs.*
 from v_sprint vs,
       (select compid
       from v_sprint_maj_int
-      where rank <= {rnk}
+      where rank <= {rnk} and age <= {age}
       group by compid
       having count(distinct eventid) >= {topn}) tp
 where vs.compid = tp.compid
